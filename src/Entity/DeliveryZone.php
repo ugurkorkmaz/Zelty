1a22bbb7b230b50eb2a6d09aa8ffcb7df1c11d0b
Uğur Korkmaz <korkmaz.uu@gmail.com>
2021-11-22 12:46:28 +0100

    Supported and added entity classes.

---
+++ src/Entity/DeliveryZone.php
@@ -0,0 +1,13 @@
+<?php 
+namespace UgurKorkmaz\Zelty\Entity;
+
+use UgurKorkmaz\Zelty\Exceptions\EntityException;
+use UgurKorkmaz\Zelty\Interfaces\EntityInterface;
+
+class DeliveryZone implements EntityInterface
+{
+    public function toArray(): array
+    {
+        return [];
+    }
+}
