2c8e7d9803bd3ddcd8d97c3e0e3e8757950b10ab
Uğur Korkmaz <korkmaz.uu@gmail.com>
2021-11-22 10:31:57 +0100

    Bug fixes.

--- src/Exceptions/Requestexception.php
+++ src/Exceptions/Requestexception.php
@@ -7,6 +7,7 @@ class RequestException extends Exception
 {
     public function __construct($message = "", $code = 0, Exception $previous = null)
     {
+        
         parent::__construct($message, $code, $previous);
     }
 }
